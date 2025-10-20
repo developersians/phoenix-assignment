88e3a8f89543e099398c82aca300bb56993f55c8
developersians <developersian@gmail.com>
2025-10-20 22:25:42 +0330

    init: Initial solution architect

---
+++ src/PhoenixSubs.Domain/Shared/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixSubs.Domain.Shared
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
